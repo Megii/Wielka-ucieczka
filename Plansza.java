bcfde76b3b68f71b59e8f47298a632c89e3db478
Megii <magdalena.wojciechowska@poczta.onet.pl>
2015-10-29 20:10:34 +0100

    poprawka

--- Plansza.java
+++ Plansza.java
@@ -1,22 +1,28 @@
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+
 import java.util.Timer;
-public class Plansza   {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+public class Plansza extends Applet   {
+
 	int rozmiar1=14*40;
 	int rozmiar2=10*40;
-	
+	static Applet applet;
 	Zadanie zadanie = new Zadanie();
 	Timer timer = new Timer();
 	
-
+	Image bufor;
+	Graphics bg;
 	static int stan = 1;
 
 	public void init()
 	{
 		
-		
+			applet.setSize(rozmiar1, rozmiar2);
+			applet.setBackground(Color.WHITE);
+			bufor = createImage(rozmiar1, rozmiar2);
+			bg = bufor.getGraphics();
 			timer.scheduleAtFixedRate(zadanie, 20, 20);
 			
 			
