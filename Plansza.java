3786cd5522c537ceeb0ce19d85287291968c7afd
Megii <magdalena.wojciechowska@poczta.onet.pl>
2015-11-10 14:31:37 +0100

    blad

--- Plansza.java
+++ Plansza.java
@@ -4,7 +4,7 @@ import java.awt.Graphics;
 import java.awt.Image;
 
 import java.util.Timer;
-public class Plansza extends Applet   {
+public class Plansza extends Applet  {
 
 	int rozmiar1=14*40;
 	int rozmiar2=10*40;
@@ -12,22 +12,17 @@ public class Plansza extends Applet   {
 	Zadanie zadanie = new Zadanie();
 	Timer timer = new Timer();
 	
-	Image bufor;
-	Graphics bg;
-	static int stan = 1;
-
+	
 	public void init()
 	{
 		
 			applet.setSize(rozmiar1, rozmiar2);
 			applet.setBackground(Color.WHITE);
-			bufor = createImage(rozmiar1, rozmiar2);
-			bg = bufor.getGraphics();
 			timer.scheduleAtFixedRate(zadanie, 20, 20);
 			
 			
 	}
-	
+	 
 	
 	
 }
