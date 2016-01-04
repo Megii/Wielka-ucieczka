8fd34688ed5ab610a97ddd8f16abe784f4693d94
Megii <magdalena.wojciechowska@poczta.onet.pl>
2016-01-04 20:07:31 +0100

    applet

--- src/game/Maze.java
+++ src/game/Maze.java
@@ -5,76 +5,136 @@ import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-
-
-public class Maze extends JPanel {
-
-	int[][] board={
-			{2,1,1,1,1,0,1,1,1,1,1,1,1,0,0,0,0,0,0},
-			{0,0,1,0,1,0,1,0,0,0,0,0,1,1,1,1,0,0,0},
-			{0,1,1,1,1,1,1,1,0,0,0,0,1,0,0,1,0,0,0},
-			{0,0,0,0,0,0,0,1,1,1,1,0,1,1,1,1,1,1,1},
-			{1,1,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1},
-			{1,0,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1},
-			{1,0,1,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,1},
-			{1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			{1,0,0,0,1,0,1,1,1,1,0,1,1,1,1,1,1,0,1},
-			{1,0,0,0,0,0,1,0,0,1,0,1,0,1,0,0,1,0,1},
-			{1,1,1,1,1,0,1,0,0,1,0,1,0,1,0,0,1,0,1},
-			{0,0,0,0,1,0,1,0,0,1,0,1,0,1,0,0,1,0,1},
-			{0,1,1,1,1,0,1,0,0,1,0,1,0,1,1,0,1,0,1},
-			{0,1,1,1,1,0,1,0,0,1,0,1,0,0,0,0,1,0,1},
-			{0,1,0,0,1,0,1,0,0,1,1,1,0,0,0,0,1,0,0},
-			{1,1,1,1,1,0,1,0,1,1,1,0,1,1,1,1,1,0,0},
-			{1,0,0,0,0,0,1,0,1,0,1,0,1,0,0,0,0,0,0},
-			{1,0,0,0,0,0,1,0,1,0,1,0,1,0,0,0,1,1,1},
-			{1,1,1,1,1,1,1,0,1,1,1,0,1,0,0,0,1,0,1},
-			{0,0,0,0,0,1,1,1,1,1,1,0,1,1,1,1,1,0,2}
-
-	  };
-	
-	int columns = board[0].length;
-	int rows = board.length;
-	int size = 12;
-	
-	int height = -1;
-	int width = -1;
-	int left;
-	int top;
-	int totalHeight;
-	int totalWidth;
-		
-	
-	void render(Graphics g)
-	{
-		 for(int i=0; i<rows; i++)
+public class Maze extends JPanel{
+  int board[][] = new int[19][18];
+  int wyjscia[][] = new int[19][18];
+  int x = 0;
+  int y = 0;
+ 
+  
+ 
+  boolean koniec= false;
+ 
+  public void run()
 		  {
-			  for(int j=0; j<columns;j++)
-			  {
-				  
-				  switch(board[i][j])
+	  
+			  if (koniec()== true)
 				  {
-				  	//Printing wall
-				  	case 0:
-				  		g.drawImage(Gra.wall,j*Gra.WIDTH/columns,i*Gra.HEIGHT/rows, Gra.WIDTH/columns+1, Gra.HEIGHT/rows,null);
-				  		break;
-				  	//Printing path
-				  	case 1:
-				  	
-				  		g.drawImage(Gra.floor,j*Gra.WIDTH/columns,i*Gra.HEIGHT/rows, Gra.WIDTH/columns+1, Gra.HEIGHT/rows,null);
-				  		break;
-				  	//Printing start and end
-				  	case 2:
-				  		g.setColor(Color.green);
-				  		g.fillRect(j*Gra.WIDTH/columns,i*Gra.HEIGHT/rows, Gra.WIDTH/columns+1, Gra.HEIGHT/rows);
-				  		break;
-				  
+				  Gra.stan = 2;
 				  }
-					 
+			  
+	  Gra.applet.repaint();
+		  }
+		  
+	public boolean koniec()
+		  {		
+		  
+			  for(int i=0; i<board.length; i++)
+			  {
+				  for(int j=0; j<board[0].length;j++)
+				  {
+					if ((wyjscia[i][j]==2)&&(board[i][j]!=3))  
+					{
+						return false;
+					}
+				  }
+			  }
+			  return true;
+		  }
+		  
+	public void zrobPlansze()
+		  {
+			  
+			  int board[][] = {
+					    {2,1,1,1,1,0,1,1,1,1,1,1,1,0,0,0,0,0,0},
+						{0,0,1,0,1,0,1,0,0,0,0,0,1,1,1,1,0,0,0},
+						{0,1,1,1,1,1,1,1,0,0,0,0,1,0,0,1,0,0,0},
+						{0,0,0,0,0,0,0,1,1,1,1,0,1,1,1,1,1,1,1},
+						{1,1,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1},
+						{1,0,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1},
+						{1,0,1,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,1},
+						{1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+						{1,0,0,0,1,0,1,1,1,1,0,1,1,1,1,1,1,0,1},
+						{1,0,0,0,0,0,1,0,0,1,0,1,0,1,0,0,1,0,1},
+						{1,1,1,1,1,0,1,0,0,1,0,1,0,1,0,0,1,0,1},
+						{0,0,0,0,1,0,1,0,0,1,0,1,0,1,0,0,1,0,1},
+						{0,1,1,1,1,0,1,0,0,1,0,1,0,1,1,0,1,0,1},
+						{0,1,1,1,1,0,1,0,0,1,0,1,0,0,0,0,1,0,1},
+						{0,1,0,0,1,0,1,0,0,1,1,1,0,0,0,0,1,0,0},
+						{1,1,1,1,1,0,1,0,1,1,1,0,1,1,1,1,1,0,0},
+						{1,0,0,0,0,0,1,0,1,0,1,0,1,0,0,0,0,0,0},
+						{1,0,0,0,0,0,1,0,1,0,1,0,1,0,0,0,1,1,1},
+						{1,1,1,1,1,1,1,0,1,1,1,0,1,0,0,0,1,0,1},
+						{0,0,0,0,0,1,1,1,1,1,1,0,1,1,1,1,1,0,2}
+			  };
+			  for(int i=0; i<board.length; i++)
+			  {
+				  for(int j=0; j<board[0].length;j++)
+				  {
+					  board[i][j] = board[i][j];
+				  }
+			  }
+		  
+		  
+		  for(int i=0; i<board.length; i++)
+		  {
+			  for(int j=0; j<board[0].length;j++)
+			  {
+				  if(board[i][j]==2)
+				  {
+					  board[i][j] = 0;
+					  wyjscia[i][j] = 2;
+				  }
 			  }
 		  }
-	}
-	
-	
-	
+	  }
+		  
+		  public void ruszaj(char gdzie)
+		  {
+			  switch(gdzie)
+			  {
+			  case 'l':
+				  if(board[x][y-1]==1)
+					{
+						board[x][y-1]=2;
+						board[x][y-1]=1;
+						y--;
+					}	
+				
+				  break;
+			  case 'p':
+				  if(board[x][y+1]==1)
+					{
+						board[x][y+1]=2;
+						board[x][y+1]=1;
+						y++;
+					}	
+				  break;
+			  case'g':
+				  if(board[x+1][y]==1)
+					{
+						board[x+1][y]=2;
+						board[x+1][y]=1;
+						x++;
+					}	
+				  break;
+			  case 'd':
+				  if(board[x-1][y]==1)
+					{
+						board[x-1][y]=2;
+						board[x-1][y]=1;
+						x--;
+					}	
+				  break;
+			  }
+
+		  }
+			
+			
+			
 }
+
+
+
+
+
